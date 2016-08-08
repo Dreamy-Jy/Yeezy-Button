90b3563ebcf3d389b59ad5b1beed7238ba3b3afa
jthomasASC <thomasjordane7@gmail.com>
2016-08-07 21:07:21 -0400

    Final update

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ var audio = new Audio('YeezyTalk.mp3');
 function kpressed(){
 	audio.play();
 }
-/*
-function preload(){
-	sound = loadSound('/hello.mp3');
-}
-
-function setup(){
-	
-	sound.setVolume(0.1);
-}
-function draw(){
-	sound.play();
-}
-function kpressed(){
-	sound.play();
-}*/
